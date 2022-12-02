e813d12dd84896a0a1a2f503a720c0193e960d08
Aurelio Rojas Eng <42392895+aureng@users.noreply.github.com>
2022-12-02 15:20:02 -0600

    Add files via upload

---
+++ Headers/Window.h
@@ -0,0 +1,46 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+	GLfloat getmuevecoche() { return muevecoche; }
+	GLfloat getmuevekitt() { return muevekitt; }
+	GLfloat getmuevecofreaudi() { return muevecofreaudi;}
+	GLfloat getmuevecofrekitt() { return muevecofrekitt; }
+	GLboolean getmovimientoHelicoptero() { return movimientoHelicoptero; }
+	GLboolean getBandera() { return bandera; }
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+	
+	~Window();
+private: 
+	GLFWwindow *mainWindow;
+	GLint width, height;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex,muevecoche,muevekitt,muevecofreaudi, muevecofrekitt;
+	GLboolean movimientoHelicoptero, bandera;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
